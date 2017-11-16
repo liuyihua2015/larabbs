fd0d9cec4f2d539fd47aac654ce9729adc00d2b7
liuiyhua <liuyihua2015@sina.com>
2017-11-16 09:07:42 +0000

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/u/2377197351" target="_blank">liuyihua
+            </a> 设计和编码 <span style="color: #e27575;font-size: 14px;"></span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:liuyihua2015@sina.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','首页')
+
+@section('content')
+<h1>this is  home</h1>
+@stop
